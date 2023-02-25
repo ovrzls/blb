168c2521a2aa453b3802d565d543da70c07ec842
Zelis <mark@markzelis.com>
2023-02-25 15:17:49 -0500

    fix error in php date

--- dist/incs/foot.php
+++ dist/incs/foot.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>&copy;<?php echo date(Y); ?>, <a href="mailto:mark@microrebellion.com">Mark Zelis</a></p>
+    <p>&copy;<?php echo date('Y'); ?>, <a href="mailto:mark@microrebellion.com">Mark Zelis</a></p>
 </footer>
 </body>
 </html>
